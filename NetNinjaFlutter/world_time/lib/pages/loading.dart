959884785af8025f4a6c7c99160842f0d410079e
Rajesh Narayana Rao <mailrbn@gmail.com>
2023-05-17 18:38:03 +1000

    Final commit

---
+++ NetNinjaFlutter/world_time/lib/pages/loading.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime('Europe/Berlin', 'Berlin', 'germany.png');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitCubeGrid(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
